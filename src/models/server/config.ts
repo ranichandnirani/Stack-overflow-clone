40f82b205f30364f5d55cbd5c7d54c7b95eff18c
Chandni Rani <chandnirani229@gmail.com>
2026-09-01 23:59:01 +0530

    Project setup and start data modlling

---
+++ src/models/server/config.ts
@@ -0,0 +1,19 @@
+import env from "@/app/env";
+
+import { Client, Avatars, Databases, Storage, Users } from "node-appwrite";
+
+let client = new Client();
+
+client
+    .setEndpoint(env.appwrite.endpoint) // Your API Endpoint
+    .setProject(env.appwrite.projectId) // Your project ID
+    .setKey(env.appwrite.apiKey) // Your secret API key
+     
+;
+
+const databases = new Databases(client);
+const users = new Users(client);
+const avatars = new Avatars(client);
+const storage = new Storage(client);
+
+export { client, databases, users, avatars, storage };
